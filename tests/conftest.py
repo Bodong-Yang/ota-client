1a69028039192f7f2d4ad6b3b1ac6ce9afbab296
Bodong Yang <bodong.yang@tier4.jp>
2022-06-01 12:55:12 +0900

    append /ota-client/app to sys.path by PYTHONPATH

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,5 @@
 import pytest
-import sys
-
 from pathlib import Path
-
-
-@pytest.fixture(autouse=True, scope="session")
-def pythonpath():
-    _base_dir = Path(__file__).absolute().parent.parent
-    sys.path.extend([str(_base_dir), str(_base_dir / "app")])
-
 
 # not enable proxy when doing test
 DEFUALT_PROXY_INFO = """
