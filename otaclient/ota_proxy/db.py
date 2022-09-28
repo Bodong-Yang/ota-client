191e2fc42a12e22d400ad74cb9499d6dfb9da2b2
Bodong Yang <bodong.yang@tier4.jp>
2022-09-28 09:20:44 +0000

    ota_proxy.db: update according to orm

--- otaclient/ota_proxy/db.py
+++ otaclient/ota_proxy/db.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from functools import partial
 from pathlib import Path
-from typing import Any, List, Optional, Type, TypeVar, Callable
+from typing import Any, List, Optional, Type, Callable, cast
 
 from .config import config as cfg
 from ._orm import ColumnDescriptor, ORMBase
@@ -35,9 +35,8 @@ class CacheMeta(ORMBase):
     content_encoding: ColumnDescriptor[str] = ColumnDescriptor(str, "TEXT")
 
 
-class OTACacheDB:
+class _OTACacheDB:
     TABLE_NAME: str = cfg.TABLE_NAME
-    ROW_TYPE = CacheMeta
     OTA_CACHE_IDX: List[str] = [
         cfg.BUCKET_LAST_ACCESS_IDX,
     ]
@@ -84,7 +83,7 @@ class OTACacheDB:
                     logger.warning(f"{self.TABLE_NAME} not found, init db...")
                     # create ota_cache table
                     con.execute(
-                        self.ROW_TYPE.get_create_table_stmt(self.TABLE_NAME),
+                        CacheMeta.get_create_table_stmt(self.TABLE_NAME),
                         (),
                     )
 
@@ -109,11 +108,11 @@ class OTACacheDB:
     def remove_entries_by_field(self, fd: ColumnDescriptor, *_inputs: Any) -> int:
         if not _inputs:
             return 0
-        if self.ROW_TYPE.contains_field(fd) and fd.check_type(_inputs[0]):
+        if CacheMeta.contains_field(fd) and fd.check_type(_inputs[0]):
             with self._con as con:
                 _regulated_input = [(i,) for i in _inputs]
                 cur = con.executemany(
-                    f"DELETE FROM {self.TABLE_NAME} WHERE {fd.field_name}=?",
+                    f"DELETE FROM {self.TABLE_NAME} WHERE {fd._field_name}=?",
                     _regulated_input,
                 )
                 return cur.rowcount
@@ -122,11 +121,11 @@ class OTACacheDB:
     def lookup_entry_by_field(
         self, fd: ColumnDescriptor, _input: Any
     ) -> Optional[CacheMeta]:
-        if not self.ROW_TYPE.contains_field(fd) or fd.check_type(_input):
+        if not CacheMeta.contains_field(fd) or fd.check_type(_input):
             return
         with self._con as con:
             cur = con.execute(
-                f"SELECT * FROM {self.TABLE_NAME} WHERE {fd.field_name}=?",
+                f"SELECT * FROM {self.TABLE_NAME} WHERE {fd.name}=?",
                 (_input,),
             )
             if row := cur.fetchone():
@@ -134,8 +133,8 @@ class OTACacheDB:
                 res = CacheMeta.row_to_meta(row)
                 cur = con.execute(
                     (
-                        f"UPDATE {self.TABLE_NAME} SET {self.ROW_TYPE.last_access.field_name}=? "  # type: ignore
-                        f"WHERE {self.ROW_TYPE.url.field_name}=?"  # type: ignore
+                        f"UPDATE {self.TABLE_NAME} SET {CacheMeta.last_access.name}=? "  # type: ignore
+                        f"WHERE {CacheMeta.url.name}=?"  # type: ignore
                     ),
                     (int(datetime.now().timestamp()), res.url),
                 )
@@ -146,7 +145,7 @@ class OTACacheDB:
             return 0
         with self._con as con:
             cur = con.executemany(
-                f"INSERT OR REPLACE INTO {self.TABLE_NAME} VALUES ({self.ROW_TYPE.get_shape()})",
+                f"INSERT OR REPLACE INTO {self.TABLE_NAME} VALUES ({CacheMeta.get_shape()})",
                 [m.to_tuple() for m in cache_meta],
             )
             return cur.rowcount
@@ -168,8 +167,8 @@ class OTACacheDB:
                 or None if no enough entries for space reserving.
         """
         bucket_fn, last_access_fn = (
-            self.ROW_TYPE.bucket.field_name,  # type: ignore
-            self.ROW_TYPE.last_access.field_name,  # type: ignore
+            CacheMeta.bucket.name,  # type: ignore
+            CacheMeta.last_access.name,  # type: ignore
         )
         # first, check whether we have required number of entries in the bucket
         with self._con as con:
@@ -226,28 +225,18 @@ def _proxy_wrapper(attr_n: str) -> Callable:
 
     def _wrapped(self, *args, **kwargs):
         # get the handler from underlaying db connector
-        def _inner():
-            _db = self._thread_local.db
-            f = partial(getattr(_db, attr_n), *args, **kwargs)
-            return f()
-
-        # inner is dispatched to the db connection threadpool
-        fut: Future = self._executor.submit(_inner)
+        _db_conn: _OTACacheDB = self._thread_local.db
+        fut: Future = self._executor.submit(
+            partial(getattr(_db_conn, attr_n), *args, **kwargs)
+        )
         return fut.result()
 
     return _wrapped
 
 
-_GENERIC_CLS = TypeVar("_GENERIC_CLS")
-_WRAPPED_CLS = TypeVar("_WRAPPED_CLS")
-
-
 def _proxy_cls_factory(
-    cls: Type[_GENERIC_CLS],
-    *,
-    wrapper: Callable[[str], Callable],
-    target: Type[_GENERIC_CLS],
-) -> Type[_WRAPPED_CLS]:
+    proxy_cls: Type, *, wrapper: Callable[[str], Callable], source_cls: Type
+) -> Type:
     """A proxy class factory that wraps all public methods with <wrapper>.
 
     Args:
@@ -258,16 +247,16 @@ def _proxy_cls_factory(
     Returns:
         A new proxy class for <target>.
     """
-    for attr_n, attr in target.__dict__.items():
+    for attr_n, attr in source_cls.__dict__.items():
         if not attr_n.startswith("_") and callable(attr):
-            # override method
-            setattr(cls, attr_n, wrapper(attr_n))
+            # assigned wrapped method to the proxy cls
+            setattr(proxy_cls, attr_n, wrapper(attr_n))
 
-    return cls
+    return proxy_cls
 
 
-@partial(_proxy_cls_factory, wrapper=_proxy_wrapper, target=OTACacheDB)
-class DBProxy:
+@partial(_proxy_cls_factory, wrapper=_proxy_wrapper, source_cls=_OTACacheDB)
+class _ProxyBase:
     """A proxy class for OTACacheDB that dispatches all requests into a threadpool."""
 
     def __init__(self, db_f: str):
@@ -276,13 +265,17 @@ class DBProxy:
 
         def _initializer():
             """Init a db connection for each thread worker"""
-            self._thread_local.db = OTACacheDB(db_f, init=False)
+            # NOTE: set init to False always as we only operate db when using proxy
+            self._thread_local.db = _OTACacheDB(db_f, init=False)
 
-        self._executor = ThreadPoolExecutor(max_workers=6, initializer=_initializer)
+        self._executor = ThreadPoolExecutor(max_workers=3, initializer=_initializer)
 
     def close(self):
         self._executor.shutdown(wait=True)
 
 
 # cleanup namespace
-del _make_cachemeta_cls, _proxy_cls_factory, _proxy_wrapper
+del _proxy_cls_factory, _proxy_wrapper
+
+# expose OTACacheDB classs
+OTACacheDB = cast(Type[_OTACacheDB], _ProxyBase)
