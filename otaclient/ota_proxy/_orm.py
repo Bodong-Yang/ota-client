7099155b955fad1d01625135fb169ca14bb56bb4
Bodong Yang <bodong.yang@tier4.jp>
2022-09-28 08:51:12 +0000

    ota_proxy.orm: major update

--- otaclient/ota_proxy/_orm.py
+++ otaclient/ota_proxy/_orm.py
@@ -8,65 +8,76 @@ SQLITE_DATATYPES = Union[
     float,  # REAL
     bytes,  # BLOB
     bool,  # INTEGER 0, 1
-    type(None),  # NULL
+    # NOTE: not introduce NULL type now!
+    # type(None),  # NULL
 ]
 FV = TypeVar("FV", bound=SQLITE_DATATYPES)  # field value type
 
 
-class Column(Generic[FV]):
+@dataclass
+class ColumnDescriptor(Generic[FV]):
     type_guard: bool
     field_type: Type[FV]
 
     def __init__(
-        self, field_type: Type[FV], constrains: str, *, type_guard=False
+        self,
+        field_type: Type[FV],
+        constrains: str,
+        *,
+        type_guard=False,
+        default=None,
     ) -> None:
         self.type_guard = type_guard
         self.field_type = field_type
         self.constrains = constrains
+        self._default = default if default is not None else self.field_type()
         super().__init__()
 
-    def __get__(self, obj, objtype=None) -> Union[FV, "Column"]:
+    def __get__(self, obj, objtype=None) -> Union[FV, "ColumnDescriptor"]:
         if obj is not None:
             return getattr(obj, self.private_name)
+        if obj is None and objtype:
+            # NOTE: align to the behavior of dataclass setting default value:
+            #       To determine whether a field contains a default value,
+            #       dataclasses will call the descriptor’s __get__ method
+            #       using its class access form (i.e. descriptor.__get__(obj=None, type=cls).
+            #       If the descriptor returns a value in this case,
+            #       it will be used as the field’s default.
+            return self._default
         return self
 
-    def __set__(self, obj, value: Any):
-        # during dataclass default value initializing,
-        # or input type is Type[None](NULL)
-        if isinstance(value, type(self)) or value is None:
-            value = self.field_type()  # type: ignore
+    def __set__(self, obj, value):
         if self.type_guard and not isinstance(value, self.field_type):
             raise TypeError(f"type_guard: expect {self.field_type}, get {type(value)}")
         # apply default type conversion or type default value
-        setattr(obj, self.private_name, value)
+        setattr(obj, self.private_name, self.field_type(value))  # type: ignore
 
     def __set_name__(self, owner: Type[Any], name: str):
         self.field_name = name
         self.private_name = f"_{name}"
 
+    def check_type(self, value: Any) -> bool:
+        return isinstance(value, self.field_type)
+
 
 @dataclass
 class ORMBase(Generic[FV]):
     @classmethod
-    def get_columns(cls) -> List[str]:
-        return [f.name for f in fields(cls)]
-
-    @classmethod
-    def get_shape(cls) -> str:
-        return ",".join(["?"] * len(fields(cls)))
-
-    @classmethod
     def row_to_meta(cls, row: Optional[Dict[str, FV]]):
         if not row:  # return empty cachemeta on empty input
             return cls()
-        # filter away unexpected columns
-        _parsed_row = {k: v for k, v in row.items() if k in set(cls.get_columns())}
+        # only pick recongized cols' value
+        _parsed_row = {}
+        for field in fields(cls):
+            field_name = field.name
+            if field_name in row:
+                _parsed_row[field_name] = row[field_name]
         return cls(**_parsed_row)
 
     @classmethod
     def get_create_table_stmt(cls, table_name: str) -> str:
-        _col_descriptors: List[Column] = [
-            getattr(cls, field_name) for field_name in set(cls.get_columns())
+        _col_descriptors: List[ColumnDescriptor] = [
+            getattr(cls, field.name) for field in fields(cls)
         ]
         return (
             f"CREATE TABLE {table_name}("
@@ -75,6 +86,26 @@ class ORMBase(Generic[FV]):
             )
             + ")"
         )
+
+    @classmethod
+    def get_col(cls, name: str) -> Optional[ColumnDescriptor]:
+        try:
+            return getattr(cls, name)
+        except AttributeError:
+            return
+
+    @classmethod
+    def contains_field(cls, _input: Union[str, ColumnDescriptor]) -> bool:
+        try:
+            if isinstance(_input, ColumnDescriptor):
+                _input = _input.field_name
+            return isinstance(getattr(cls, _input), ColumnDescriptor)
+        except AttributeError:
+            return False
+
+    @classmethod
+    def get_shape(cls) -> str:
+        return ",".join(["?"] * len(fields(cls)))
 
     def to_tuple(self) -> Tuple[FV]:
         return astuple(self)
